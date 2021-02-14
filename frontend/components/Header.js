afb441812898e3d4dc19dbe454e62c0798d320dc
kharioki <antolisah@gmail.com>
2021-02-14 23:00:54 +0300

    headeer component and globalstyles

---
+++ frontend/components/Header.js
@@ -0,0 +1,48 @@
+import Link from 'next/link';
+import Nav from './Nav';
+import styled from 'styled-components';
+
+const Logo = styled.h1`
+    background: var(--red, red);
+    font-size: 4rem;
+    margin-left: 2rem;
+    z-index: 2;
+    transform: skew(-7deg);
+    a {
+        color: white;
+        text-decoration: none;
+        text-transform: uppercase;
+        padding: 0.5rem 1rem;
+    }
+`;
+
+const HeaderStyles = styled.header`
+    .bar {
+        border-bottom: 10px solid var(--black, black);
+        display: grid;
+        grid-template-columns: auto 1fr;
+        justify-content: space-between;
+        align-items: center;
+    }
+    .sub-bar {
+        display: grid;
+        grid-template-columns: auto 1fr;
+        border-bottom: 1px solid var(--black, black);
+    }
+`;
+
+export default function Header() {
+    return(
+        <HeaderStyles>
+            <div className="bar">
+                <Logo>
+                    <Link href="/">Sick Fits</Link>
+                </Logo>
+            </div>
+            <div className="sub-bar">
+                <p>Search</p>
+            </div>
+            <Nav />
+        </HeaderStyles>
+    );
+}
