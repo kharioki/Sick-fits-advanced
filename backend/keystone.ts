d7bc24811cbfaea2141640d632bdba4525f5cd7e
kharioki <antolisah@gmail.com>
2021-02-17 03:51:42 +0300

    creating the user datatype

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -1,6 +1,8 @@
 import { config, createSchema } from '@keystone-next/keystone/schema';
 import 'dotenv/config';
 
+import { User } from './schemas/User';
+
 const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
 
 // session config
@@ -23,6 +25,7 @@ export default config({
     },
     lists: createSchema({
         //schema items go in here
+        User,
     }),
     ui: {
         // TODO: change this for roles
