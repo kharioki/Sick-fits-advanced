dbb862c4f5bf21db602f9b1a73d72df943ff47c0
Tony Kharioki <antolisah@gmail.com>
2021-06-08 22:35:57 +0300

    styling single page item

--- frontend/components/SingleProduct.js
+++ frontend/components/SingleProduct.js
@@ -1,6 +1,24 @@
 import { useQuery } from '@apollo/client';
 import gql from 'graphql-tag';
+import Head from 'next/head';
+import styled from 'styled-components';
+
 import DisplayError from './ErrorMessage';
+
+const ProductStyles = styled.div`
+  display: grid;
+  grid-auto-columns: 1fr;
+  grid-auto-flow: column;
+  max-width: var(--maxwidth);
+  justify-content: center;
+  align-items: top;
+  gap: 2rem;
+
+  img {
+    width: 100%;
+    object-fit: contain;
+  }
+`;
 
 const SINGLE_ITEM_QUERY = gql`
   query SINGLE_ITEM_QUERY($id: ID!) {
@@ -14,8 +32,7 @@ const SINGLE_ITEM_QUERY = gql`
         id
         image {
           id
-          filename
-          publicUrlTransformed 
+          publicUrlTransformed
         }
         altText
       }
@@ -32,9 +49,20 @@ export default function SingleProduct({ id }) {
 
   if (loading) return <p>Loading...</p>;
   if (error) return <DisplayError error={error} />;
+  const { Product } = data;
   return (
-    <div>
-      <h2>{data.Product.name}</h2>
-    </div>
+    <ProductStyles>
+      <Head>
+        <title>Sick Fits | {Product.name}</title>
+      </Head>
+      <img
+        src={Product.photo.image.publicUrlTransformed}
+        alt={Product.photo.altText}
+      />
+      <div className="details">
+        <h2>{Product.name}</h2>
+        <p>{Product.description}</p>
+      </div>
+    </ProductStyles>
   );
 }
