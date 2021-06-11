8b59fab50008beed92a04e1286979795d1aea0a0
Tony Kharioki <antolisah@gmail.com>
2021-06-11 17:46:35 +0300

    signout flow

---
+++ frontend/components/SignOut.js
@@ -0,0 +1,20 @@
+import { gql, useMutation } from '@apollo/client';
+import { CURRENT_USER_QUERY } from './User';
+
+export const SIGNOUT_MUTATION = gql`
+  mutation {
+    endSession
+  }
+`;
+
+export default function SignOut() {
+  const [signout] = useMutation(SIGNOUT_MUTATION, {
+    refetchQueries: [{ query: CURRENT_USER_QUERY }],
+  });
+
+  return (
+    <button type="button" onClick={signout}>
+      Sign Out
+    </button>
+  );
+}
